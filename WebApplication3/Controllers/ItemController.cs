28ad363b691786b6c3e919109dfbf24f38d9a132
Melissa Paradiso <meltheeclipsequeen@gmail.com>
2018-10-18 11:52:39 -0500

    built instance

--- WebApplication3/Controllers/ItemController.cs
+++ WebApplication3/Controllers/ItemController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MidtermExam2.Models;
-using MidtermExam2.Views;
 
 namespace MidtermExam2.Controllers
 {
--- WebApplication3/Models/ItemRepository.cs
+++ WebApplication3/Models/ItemRepository.cs
@@ -6,13 +6,27 @@ using System.Threading.Tasks;
 
 namespace MidtermExam2.Models
 {
-    public class ItemRepository
+    interface IItemRepository
     {
-        private static List<Item> itemlist = new List<Item>();
-
-        public static IQueryable<ItemRepository> Items;
-        
-          
-
+         string ItemRepository();
     }
+    public class ItemRepository{ 
+        int ItemID
+        {
+            get; set;
+        }
+        string Description
+        {
+            get; set;
+        }
+    }
+
+        public class InMemoryItemRepository : IQueryable<Item>
+        {
+            public int ItemID { get; set; }
+            public string Description { get; set; }
+            public int UnitsOnHand { get; set; }
+            public decimal Cost { get; set; }
+            public decimal SellingPrice { get; set; }
+        }
 }
